9ccd7538d35439c171ee8be6ad72f3ce3f7b2dfa
Heine Furubotten <hfurubotten@gmail.com>
2014-10-27 00:04:22 +0100

    improved git member object

--- git/members.go
+++ git/members.go
@@ -2,9 +2,8 @@ package git
 
 import (
 	"encoding/gob"
+	"errors"
 	"log"
-	"fmt"
-	"crypto/sha256"
 
 	"code.google.com/p/goauth2/oauth"
 	"github.com/google/go-github/github"
@@ -12,79 +11,142 @@ import (
 )
 
 func init() {
-	gob.Register(member{})
+	gob.Register(Member{})
 }
 
-var store = diskv.New(diskv.Options{
+var userstore = diskv.New(diskv.Options{
+	BasePath:     "diskv/users/",
 	CacheSizeMax: 1024 * 1024 * 256,
 })
 
-type member struct {
+type Member struct {
 	githubclient *github.Client
 	Username     string
 	Name         string
 	StudentID    int
+	IsTeacher    bool
 
-	AccessToken string
+	accessToken token
 }
 
-func NewMember(oauthtoken string) member {
-	t := &oauth.Transport{
-		Token: &oauth.Token{AccessToken: oauthtoken},
+func NewMember(oauthtoken string) (m Member) {
+	m = Member{accessToken: NewToken(oauthtoken)}
+
+	var err error
+	if m.accessToken.HasTokenInStore() {
+		m.Username, err = m.accessToken.GetUsernameFromTokenInStore()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else {
+		err = m.loadDataFromGithub()
+		if err != nil {
+			log.Println(err)
+			return
+		}
 	}
-	g := github.NewClient(t.Client())
-	return member{githubclient: g, AccessToken: oauthtoken}
-}
 
-func (m *member) LoadData() (err error) {
-	if hash := sha256.Sum256([]byte(m.AccessToken)); store.Has(fmt.Sprintf("%x", hash)) {
-		var tmp member
-
-    	err = store.ReadGob(m.Username, &tmp, false)
-    	m.Copy(tmp)
-
-    	return
-	}
-
-	user, _, err := m.githubclient.Users.Get("")
+	err = m.loadData()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	if user.Login != nil {
-		m.Username = *user.Login
-	}
+	return
+}
 
-	if user.Name != nil {
-		m.Name = *user.Name
+func (m *Member) loadDataFromGithub() (err error) {
+		err = m.connectToGithub()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		user, _, err := m.githubclient.Users.Get("")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if user.Login != nil {
+			m.Username = *user.Login
+		}
+
+		if user.Name != nil {
+			m.Name = *user.Name
+		}
+
+		return
+}
+
+func (m *Member) loadData() (err error) {
+	if userstore.Has(m.Username) {
+		var tmp Member
+
+    	err = userstore.ReadGob(m.Username, &tmp, false)
+    	if err != nil {
+    		log.Println(err)
+    		return
+    	}
+    	m.Copy(tmp)
+
+    	if !m.accessToken.HasTokenInStore() {
+    		m.accessToken.SetUsernameToTokenInStore(m.Username)
+    	}
 	}
 
 	return
 }
 
-func (m member) StickToSystem() (err error) {
-	hash := sha256.Sum256([]byte(m.AccessToken))
-    err = store.WriteGob(fmt.Sprintf("%x", hash), m)
-
-    return 
+func (m Member) StickToSystem() (err error) {
+    return userstore.WriteGob(m.Username, m)
 }
 
-func (m *member) FetchFromSystem(accesstoken string) (err error) {
-
-	var tmp member
-
-	hash := sha256.Sum256([]byte(accesstoken))
-    err = store.ReadGob(fmt.Sprintf("%x", hash), &tmp, false)
-    m.Copy(tmp)
-
-    return
-}
-
-func (m *member) Copy(tmp member){
+func (m *Member) Copy(tmp Member){
 	m.Username     = tmp.Username
 	m.Name         = tmp.Name
 	m.StudentID    = tmp.StudentID
+	m.IsTeacher    = tmp.IsTeacher
+}
 
-	m.AccessToken  = tmp.AccessToken
+func (m Member) IsComplete() bool {
+	if m.Name == "" || m.StudentID == 0 || m.Username == "" {
+		return false
+	}
+
+	return true
+}
+
+func (m *Member) connectToGithub() error {
+	if m.githubclient != nil {
+		return nil
+	}
+
+	if !m.accessToken.HasToken() {
+		return errors.New("Missing AccessToken to the memeber. Can't contact github.")
+	}
+
+	t := &oauth.Transport{
+		Token: &oauth.Token{AccessToken: m.accessToken.GetToken()},
+	}
+	m.githubclient = github.NewClient(t.Client())
+	return nil
+}
+
+func (m *Member) ListOrgs() (ls []string, err error) {
+	err = m.connectToGithub()
+	if err != nil {
+		return
+	}
+
+	orgs, _, err := m.githubclient.Organizations.List("", nil)
+
+	ls = make([]string, len(orgs))
+
+	for i, org := range orgs {
+		ls[i] = *org.Login
+	}
+
+	return 
 }
