677f4998e8766a5ac0ba389f23c597ec299bb43d
Heine Furubotten <hfurubotten@gmail.com>
2015-01-07 03:16:26 +0100

    Group object

---
+++ git/groups.go
@@ -0,0 +1,58 @@
+package git
+
+import (
+	"strconv"
+
+	"github.com/hfurubotten/autograder/global"
+	"github.com/hfurubotten/diskv"
+)
+
+type Group struct {
+	ID            int
+	Active        bool
+	Course        string
+	Members       map[string]interface{}
+	CurrentLabNum int
+
+	store *diskv.Diskv
+}
+
+func NewGroup(org string, groupnum int) (g Group, err error) {
+	store := GetGroupStore(org)
+	num := strconv.Itoa(groupnum)
+
+	if store.Has(num) {
+		err = store.ReadGob(num, &g, false)
+		if err != nil {
+			return
+		}
+		g.store = store
+		return
+	}
+
+	g = Group{
+		ID:            groupnum,
+		Active:        false,
+		Course:        org,
+		Members:       make(map[string]interface{}),
+		CurrentLabNum: 1,
+		store:         store,
+	}
+
+	return
+}
+
+func (g *Group) AddMember(user string) {
+	g.Members[user] = nil
+}
+
+func (g Group) StickToSystem() error {
+	return g.store.WriteGob(strconv.Itoa(g.ID), g)
+}
+
+func GetGroupStore(org string) *diskv.Diskv {
+	return diskv.New(diskv.Options{
+		BasePath:     global.Basepath + "diskv/groups/" + org + "/",
+		CacheSizeMax: 1024 * 1024 * 256,
+	})
+}
