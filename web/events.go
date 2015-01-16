b8e1ad8233ea877b477c93712bbda5f03bc8ddd6
Heine Furubotten <hfurubotten@gmail.com>
2015-01-16 08:55:56 +0100

    Web: Adapted event handler to handle new CI Daemon.

--- web/events.go
+++ web/events.go
@@ -3,18 +3,68 @@ package web
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	ci "github.com/hfurubotten/autograder/ci"
 	"github.com/hfurubotten/autograder/git"
 )
 
 func webhookeventhandler(w http.ResponseWriter, r *http.Request) {
-	payload, err := git.DecodeHookPayload(r.Body)
+	load, err := git.DecodeHookPayload(r.Body)
 	if err != nil {
 		log.Println("Error: ", err)
 	}
 
-	go ci.StartTesterDeamon(payload)
+	if !git.HasMember(load.User) {
+		log.Println("Not a valid user: ", load.User)
+		return
+	}
 
-	log.Println(payload)
+	if !git.HasOrganization(load.Organization) {
+		log.Println("Not a valid org: ", load.Organization)
+		return
+	}
+
+	org := git.NewOrganization(load.Organization)
+	user := git.NewMemberFromUsername(load.User)
+
+	isgroup := !strings.Contains(load.Repo, "-"+git.STANDARD_REPO_NAME)
+
+	var labfolder string
+	var labnum int
+	var username string
+	if isgroup {
+		gnum, err := strconv.Atoi(load.Repo[len("group"):])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		group, err := git.NewGroup(org.Name, gnum)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		labnum = group.CurrentLabNum
+		labfolder = org.GroupLabFolders[labnum]
+		username = load.Repo
+	} else {
+		labnum = user.Courses[org.Name].CurrentLabNum
+		labfolder = org.IndividualLabFolders[labnum]
+		username = strings.TrimRight(load.Repo, "-"+git.STANDARD_REPO_NAME)
+	}
+
+	opt := ci.DaemonOptions{
+		Org:          org.Name,
+		User:         username,
+		Repo:         load.Repo,
+		BaseFolder:   org.CI.Basepath,
+		LabFolder:    labfolder,
+		AdminToken:   org.AdminToken,
+		MimicLabRepo: true,
+	}
+
+	go ci.StartTesterDaemon(opt)
 }
