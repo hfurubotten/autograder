26f35cf72c6bf3c0049e75aa3fc8bb36d01771a8
Heine Furubotten <hfurubotten@gmail.com>
2015-03-03 20:07:00 +0100

    web: new error messages

--- web/errormsgs.go
+++ web/errormsgs.go
@@ -1,20 +1,18 @@
 package web
 
-type errMsg struct {
-	Error string
-}
-
 type JSONErrorMsg struct {
 	Error    bool   `json:Error`
 	ErrorMsg string `json:ErrorMsg`
 }
 
 var (
-	ErrSignIn              = &errMsg{"xx You are not signed in. Please sign in to preform actions."}
-	ErrAccessToken         = &errMsg{"Couldn't get your access token. Try to sign in again."}
-	ErrNotAdmin            = &errMsg{"You are not a administrator. If you infact are an administrator, try to sign in again."} // TODO Fix text
-	ErrMissingField        = &errMsg{"Missing required parameters."}                                                           // TODO Fix text
-	ErrInvalidAdminField   = &errMsg{"Can't use admin parameters."}                                                            // TODO Fix text
-	ErrInvalidTeacherField = &errMsg{"Can't use teacher parameters."}                                                          // TODO Fix text
-	ErrNotStored           = &errMsg{"Edit not stored in system."}
+	ErrSignIn              = &JSONErrorMsg{true, "You are not signed in. Please sign in to preform actions."}
+	ErrAccessToken         = &JSONErrorMsg{true, "Couldn't get your access token. Try to sign in again."}
+	ErrNotMember           = &JSONErrorMsg{true, "You are not a member of this course."}
+	ErrNotAdmin            = &JSONErrorMsg{true, "You are not a administrator. If you infact are an administrator, try to sign in again."} // TODO Fix text
+	ErrMissingField        = &JSONErrorMsg{true, "Missing required parameters."}                                                           // TODO Fix text
+	ErrInvalidAdminField   = &JSONErrorMsg{true, "Can't use admin parameters."}                                                            // TODO Fix text
+	ErrInvalidTeacherField = &JSONErrorMsg{true, "Can't use teacher parameters."}                                                          // TODO Fix text
+	ErrNotStored           = &JSONErrorMsg{true, "Edit not stored in system."}
+	ErrUnknownCourse       = &JSONErrorMsg{true, "Unknown course."}
 )
