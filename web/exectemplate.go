9539eb28d74ff7890aa536202c5475b79b2a8e7d
Heine Furubotten <hfurubotten@gmail.com>
2015-03-09 22:40:11 +0100

    web: added noescape func on strings and time

--- web/exectemplate.go
+++ web/exectemplate.go
@@ -4,10 +4,21 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
+var funcMap = template.FuncMap{
+	"noescape": func(s string) template.HTML {
+		return template.HTML(s)
+	},
+	"noescapetime": func(t time.Time) template.HTML {
+		return template.HTML(t.String())
+	},
+}
+
 func execTemplate(page string, w http.ResponseWriter, view interface{}) {
-	t, err := template.ParseFiles(htmlBase+page, htmlBase+"template.html")
+	t := template.New("template").Funcs(funcMap)
+	t, err := t.ParseFiles(htmlBase+page, htmlBase+"template.html")
 	if err != nil {
 		log.Printf("Error parsing %s: %v", page, err)
 		return
