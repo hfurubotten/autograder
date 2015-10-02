835640461357e8bd85c72c86e45c099311a67e40
Hein Meling <hein.meling@uis.no>
2015-10-02 12:52:01 -0400

    fixed a few error messages;
    stopped because i wasn't sure of their usage.

--- web/errormsgs.go
+++ web/errormsgs.go
@@ -1,8 +1,14 @@
 package web
 
+// TODO Clean up: some of these error messages seem to be less useful;
+// Or rather it should not be possible to click on things that only teachers
+// have access to when you are a student. Maybe I'm misunderstanding their use.
+// Are these errors ever shown to users in the Web UI?? Or is this just for
+// logging.
+
 // JSONErrorMsg represents the error part of a JSON reply.
 type JSONErrorMsg struct {
-	Error    bool   `json:"Error"`
+	Error    bool   `json:"Error"` //TODO Why have a boolean here?
 	ErrorMsg string `json:"ErrorMsg"`
 }
 
@@ -16,16 +22,16 @@ var (
 	// ErrNotMember is a standard JSON error reply when a user is not a member of a course.
 	ErrNotMember = &JSONErrorMsg{true, "You are not a member of this course."}
 
-	// ErrNotMember is a standard JSON error reply when a user is not a member of a course.
+	// ErrUnknownMember is a standard JSON error reply when a user is not a member of a course.
 	ErrUnknownMember = &JSONErrorMsg{true, "Unknown student."}
 
 	// ErrNotAdmin is a standard JSON error reply when a user is not a admin.
-	ErrNotAdmin = &JSONErrorMsg{true, "You are not a administrator. If you infact are an administrator, try to sign in again."} // TODO Fix text
+	ErrNotAdmin = &JSONErrorMsg{true, "You don't have administrator privileges. If you are an administrator, try to sign in again."}
 
-	// ErrNotAdmin is a standard JSON error reply when a user is not a admin.
-	ErrNotTeacher = &JSONErrorMsg{true, "You are not teaching this course."} // TODO Fix text
+	// ErrNotTeacher is a standard JSON error reply when a user is not a teacher.
+	ErrNotTeacher = &JSONErrorMsg{true, "You are not a teacher for this course."}
 
-	// ErrMissingField is a standard JSON error reply when a request is midding fields.
+	// ErrMissingField is a standard JSON error reply when a request is missing fields.
 	ErrMissingField = &JSONErrorMsg{true, "Missing required parameters."} // TODO Fix text
 
 	// ErrInvalidAdminField is a standard JSON error reply when a invalid admin field is recieved.
@@ -35,11 +41,11 @@ var (
 	ErrInvalidTeacherField = &JSONErrorMsg{true, "Can't use teacher parameters."} // TODO Fix text
 
 	// ErrNotStored is a standard JSON error reply when a storage error occures.
-	ErrNotStored = &JSONErrorMsg{true, "Edit not stored in system."}
+	ErrNotStored = &JSONErrorMsg{true, "Changes have not been stored."} // TODO should we provide guidelines
 
 	// ErrUnknownCourse is a standard JSON error reply when the course is unknown.
 	ErrUnknownCourse = &JSONErrorMsg{true, "Unknown course."}
 
-	// ErrUnknownCourse is a standard JSON error reply when the course is unknown.
+	// ErrUnknownGroup is a standard JSON error reply when the course is unknown.
 	ErrUnknownGroup = &JSONErrorMsg{true, "Unknown group."}
 )
