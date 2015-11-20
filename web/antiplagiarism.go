12f266f227a539f04e77d91ef79ca7836ba106c1
Eric Freeman <ericfree@badne7.ux.uis.no>
2015-11-20 16:51:07 +0100

    Updated results file name to point to JSON file.

--- web/antiplagiarism.go
+++ web/antiplagiarism.go
@@ -202,7 +202,7 @@ func saveNewResults(org *git.Organization, isGroup bool) {
 			// For each tool
 			for _, tool := range tools {
 				resultsDir := filepath.Join(resultsBaseDir, org.Name, lab, tool)
-				resultsFile := filepath.Join(resultsDir, "percentage.txt")
+				resultsFile := filepath.Join(resultsDir, "percentage.json")
 
 				// Get the tool's results for that lab
 				success := getFileResults(resultsFile, labIndex, tool, org, isGroup)
