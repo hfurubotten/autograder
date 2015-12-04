b2840920b94c2f156635fd18acbaaf9ba3ffd43a
Eric Freeman <ericfree@autograder2.ux.uis.no>
2015-12-04 15:53:11 +0100

    Fixed )

--- web/webserver.go
+++ web/webserver.go
@@ -48,12 +48,11 @@ func (ws Server) Start() {
 	http.Handle("/login", http.RedirectHandler(global.OAuthRedirectURL+"?client_id="+global.OAuthClientID, 307))
 	http.HandleFunc("/oauth", global.OAuthHandler)
 	http.HandleFunc(pages.SIGNOUT, auth.RemoveApprovalHandler)
-	
+
 	// Anti-plagiarism file server
 	http.Handle(resultsBaseDir,
-		http.StripPrefix(resultsBaseDir, http.FileServer(http.Dir(resultsBaseDir)))
-	)
-	
+		http.StripPrefix(resultsBaseDir, http.FileServer(http.Dir(resultsBaseDir))))
+
 	// Page handlers
 	http.HandleFunc(HomeURL, HomeHandler)
 	http.HandleFunc(ProfileURL, ProfileHandler)
